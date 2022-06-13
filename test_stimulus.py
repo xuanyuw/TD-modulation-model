648c65f01e4fd28033c76cb8e48e8479bc9e0c3c
xuanyuw <wuxuanyu86@gmail.com>
2022-06-13 18:57:19 +0800

    Update test_stimulus.py

--- test_stimulus.py
+++ test_stimulus.py
@@ -1,6 +1,32 @@
 from stimulus import Stimulus
 from calc_params import par
+import matplotlib.pyplot as plt
 
 stim = Stimulus(par)
 trial_info = stim.generate_trial()
-stim.plot_neural_input(trial_info)
+
+
+def plot_neural_input(trial_info, trial_idx):
+
+    print(trial_info['desired_output'][:, trial_idx, :].T)
+    f = plt.figure(figsize=(8, 4))
+    ax = f.add_subplot(1, 1, 1)
+    t0, t1, t2 = 0, \
+        min(par['target_time_rng']), \
+        min(par['stim_time_rng'])
+    im = ax.imshow(trial_info['neural_input'][:, trial_idx, :].T,
+                   aspect='auto', interpolation='none')
+
+    ax.set_xticks([t0, t1, t2])
+    ax.set_xticklabels(['-900', '-400', '0'])
+    f.colorbar(im, orientation='vertical')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.set_ylabel('Input Neurons')
+    ax.set_xlabel('Time relative to sample onset (ms)')
+    ax.set_title('Neural input')
+    # plt.savefig('stimulus.pdf', format='pdf')
+    plt.show()
+
+
+plot_neural_input(trial_info)
