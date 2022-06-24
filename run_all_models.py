52b3d0bf77e6320682c3b63ee0c1aa3c0043f539
xuanyuw <wuxuanyu86@gmail.com>
2022-06-24 10:13:01 +0800

    added test_acc_plots

--- run_all_models.py
+++ run_all_models.py
@@ -15,33 +15,40 @@ def try_model(par, train):
         trial(par, train=train)
 
     except KeyboardInterrupt:
-        quit('Quit by KeyboardInterrupt')
+        quit("Quit by KeyboardInterrupt")
 
 
 # Run models
-#synaptic_configs = ['full', 'fac', 'dep', 'exc_fac', 'exc_dep', 'inh_fac', 'inh_dep', 'exc_dep_inh_fac', 'none']
-synaptic_configs = ['full']
+# synaptic_configs = ['full', 'fac', 'dep', 'exc_fac', 'exc_dep', 'inh_fac', 'inh_dep', 'exc_dep_inh_fac', 'none']
+synaptic_configs = ["full"]
 for syn_config in synaptic_configs:
-    for lr in par['learning_rate_li']:
-        for rep in np.arange(par['rep_num']):
-            update_parameters({'synapse_config': syn_config,
-                               'rep': rep,
-                               'save_fn': 'model_results_%d_lr%f.pkl' % (rep, lr),
-                               'batch_size': par['train_batch_size'],
-                               'num_iterations': par['num_train_iterations'],
-                               'coherence_levels': par['train_coherence_levels'],
-                               'weight_fn': 'weight_%d_lr%f.pth' % (rep, lr),
-                               'learning_rate': lr})
-            if not exists(dirname(par['save_dir'])):
-                makedirs(dirname(par['save_dir']))
-            print('Training model %d'%rep)
+    for lr in par["learning_rate_li"]:
+        for rep in np.arange(par["rep_num"]):
+            update_parameters(
+                {
+                    "synapse_config": syn_config,
+                    "rep": rep,
+                    "save_fn": "model_results_%d_lr%f.pkl" % (rep, lr),
+                    "batch_size": par["train_batch_size"],
+                    "num_iterations": par["num_train_iterations"],
+                    "coherence_levels": par["train_coherence_levels"],
+                    "weight_fn": "weight_%d_lr%f.pth" % (rep, lr),
+                    "learning_rate": lr,
+                }
+            )
+            if not exists(dirname(par["save_dir"])):
+                makedirs(dirname(par["save_dir"]))
+            print("Training model %d" % rep)
             try_model(par, True)
-            update_parameters({'synapse_config': syn_config,
-                               'rep': rep,
-                               'save_fn': 'test_results_%d.pkl' % rep,
-                               'batch_size': par['test_batch_size'],
-                               'num_iterations': par['num_test_iterations'],
-                               'coherence_levels': par['test_coherence_levels']
-                               })
-            print('Testing model %d'%rep)
+            update_parameters(
+                {
+                    "synapse_config": syn_config,
+                    "rep": rep,
+                    "save_fn": "test_results_%d.pkl" % rep,
+                    "batch_size": par["test_batch_size"],
+                    "num_iterations": par["num_test_iterations"],
+                    "coherence_levels": par["test_coherence_levels"],
+                }
+            )
+            print("Testing model %d" % rep)
             try_model(par, False)
