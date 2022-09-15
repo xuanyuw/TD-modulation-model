2e03bab71083148f11ea8a2abe6f34c0e4c4e8fd
Xuanyu Wu <wuxuanyu86@gmail.com>
2022-09-15 18:50:17 +0800

    update

--- average_selectivity_noEI.py
+++ average_selectivity_noEI.py
@@ -4,8 +4,8 @@ import os
 from utils import *
 from types import SimpleNamespace
 
-f_dir = "crossOutput_origTimeConst_model"
-total_rep = 20
+f_dir = "crossOuput_noInterneuron_highRec_model"
+total_rep = 10
 all_lr = [0.02]
 
 
