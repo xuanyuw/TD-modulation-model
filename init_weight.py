94bb04f60a8ba914a0c0e2c020709834367f10fa
Xuanyu Wu <wuxuanyu86@gmail.com>
2022-10-10 20:07:29 +0800

    removed motion-target conn to improve stability

--- init_weight.py
+++ init_weight.py
@@ -66,10 +66,10 @@ def generate_rnn_mask():
     )
     temp_probs = np.array(
         [
-            [h_prob, m_prob, l_prob, l_prob],
-            [m_prob, h_prob, l_prob, l_prob],
-            [l_prob, l_prob, h_prob, m_prob],
-            [l_prob, l_prob, m_prob, h_prob],
+            [h_prob, m_prob, l_prob, 0],
+            [m_prob, h_prob, 0, l_prob],
+            [l_prob, 0, h_prob, m_prob],
+            [0, l_prob, m_prob, h_prob],
         ]
     )
     inh_probs = np.array(
