f39a2165ece4ce94ce3b939a377e902a9a7849bb
Xuanyu Wu <wuxuanyu86@gmail.com>
2022-11-13 17:19:38 +0800

    update

--- average_selectivity_noEI.py
+++ average_selectivity_noEI.py
@@ -4,7 +4,7 @@ import os
 from utils import *
 from types import SimpleNamespace
 
-f_dir = "crossOutput_noInterneuron_noMTConn_gaussianInOut_noCrossOut_model"
+f_dir = "crossOutput_noInterneuron_noMTConn_gaussianInOut_cross8_model"
 total_rep = 10
 all_lr = [2e-2]
 plot_sel = False
